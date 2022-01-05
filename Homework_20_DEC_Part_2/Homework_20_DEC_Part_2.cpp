7a6d6d587cfaedb6dab047ca3a13f83952bb9256
Glib Petrachkov <petrasha@gmail.com>
2022-01-05 20:44:26 +0200

    string

--- Homework_20_DEC_Part_2/Homework_20_DEC_Part_2.cpp
+++ Homework_20_DEC_Part_2/Homework_20_DEC_Part_2.cpp
@@ -925,58 +925,71 @@ int main()
 	cin >> date;
 	int month;
 	cin >> month;
-	
+	string zodiac;
 	if (month == 3 && date >= 21 && date <= 31 || month == 4 && date >= 1 && date <= 19)
 	{
-		cout << "Aries";
+		zodiac = "Aries";
+		cout << zodiac;
 	}
 	else if (month == 4 && date >= 20 && date <= 30 || month == 5 && date >= 1 && date <= 20)
 	{
-		cout << "Taurus";
+		zodiac = "Taurus";
+		cout << zodiac;
 	}
 	else if (month == 4 && date >= 20 && date <= 31 || month == 5 && date >= 1 && date <= 20)
 	{
-		cout << "Gemini";
+		zodiac = "Gemini";
+		cout << zodiac;
 	}
 	else if (month == 6 && date >= 21 && date <= 31 || month == 7 && date >= 1 && date <= 22)
 	{
-		cout << "Cancer";
+		zodiac = "Cancer";
+		cout << zodiac;
 	}
 	else if (month == 7 && date >= 23 && date <= 30 || month == 8 && date >= 1 && date <= 22)
 	{
-		cout << "Leo";
+		zodiac = "Leo";
+		cout << zodiac;
 	}
 	else if (month == 8 && date >= 23 && date <= 31 || month == 9 && date >= 1 && date <= 22)
 	{
-		cout << "Virgo";
+		zodiac = "Virgo";
+		cout << zodiac;
 	}
 	else if (month == 9 && date >= 23 && date <= 30 || month == 10 && date >= 1 && date <= 22)
 	{
-		cout << "Libra";
+		zodiac = "Libra";
+		cout << zodiac;
 	}
 	else if (month == 10 && date >= 23 && date <= 31 || month == 11 && date >= 1 && date <= 21)
 	{
-		cout << "Scorpio";
+		zodiac = "Scorpio";
+		cout << zodiac;
 	}
 	else if (month == 11 && date >= 22 && date <= 30 || month == 12 && date >= 1 && date <= 21)
 	{
-		cout << "Sagitarius";
+		zodiac ="Sagitarius";
+		cout << zodiac;
 	}
 	else if (month == 12 && date >= 22 && date <= 31 || month == 1 && date >= 1 && date <= 19)
 	{
-		cout << "Capricornus";
+		zodiac = "Capricornus";
+		cout << zodiac;
 	}
 	else if (month == 1 && date >= 20 && date <= 31 || month == 2 && date >= 1 && date <= 18)
 	{
-		cout << "Aquarius";
+		zodiac = "Aquarius";
+		cout << zodiac;
 	}
 	else if (month == 2 && date >= 19 && date <= 28 || month == 3 && date >= 1 && date <= 20)
 	{
-		cout << "Pisces";
+		zodiac = "Pisces";
+		cout << zodiac;
 	}
 	else if (month == 2 && date == 29)
 	{
-		cout << "Pisces leap year";
+		zodiac = "Pisces leap year";
+		cout << zodiac;
 	}
 	else
 	{
